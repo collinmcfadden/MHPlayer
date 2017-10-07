f3c21bedb661fa2e79a789b86a8526353993c6b9
thenerdjake <jbraun6@wisc.edu>
2017-10-07 14:06:31 -0500

    some updates before starting

--- MHPlayer.c
+++ MHPlayer.c
@@ -1,3 +1,27 @@
-//this is mhpalyer test
-//continuing test:wq
-//
+#include <stdio.h>
+#include <string.h>
+
+
+//Display for song title, artist, and maybe duration
+
+
+
+
+
+//The actual music decoding
+
+
+
+
+
+//Connecting to computer audio/output
+
+
+
+
+
+//can they add an up next song?
+
+
+
+
