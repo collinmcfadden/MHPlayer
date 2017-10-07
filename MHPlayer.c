8aa18a400addcbec028c2a5b9a7fb5f3271e9b4b
thenerdjake <jbraun6@wisc.edu>
2017-10-07 14:56:31 -0500

    added ability to take in arguements

--- MHPlayer.c
+++ MHPlayer.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 
+typedef struct track
+{
+	char title[50];
+	char artist[50];
+	char duration[50];
+	char codec[25];
+};
+
+int main(int argc, char *argv[]){
+
+if (argc < 1){
+	printf("Please choose a file to play");
+	return (0);
+}
+
+
 //Display for song title, artist, and maybe duration
 
 
@@ -22,6 +38,5 @@
 
 //can they add an up next song?
 
-
-
-
+return 0;
+}
